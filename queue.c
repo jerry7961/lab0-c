385b13cda68f28241ee891b2aa2cff7e390642c6
jerry7961 <150994926+jerry7961@users.noreply.github.com>
2024-03-18 03:38:52 +0800

    Implement q_delete_mid
    [What?] 
    Implement q_delete_mid to remove the middle element from the queue.
    [Why?]
    Removing the middle element of a queue is essential for certain algorithm. 
    [How?]
    Use slow and fast to traverse the queue. When fast reaches the end of the queue, slow points to the middle element.

--- queue.c
+++ queue.c
@@ -122,7 +122,25 @@ int q_size(struct list_head *head)
 /* Delete the middle node in queue */
 bool q_delete_mid(struct list_head *head)
 {
-    // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head)) {
+        return false;
+    }
+
+    struct list_head *slow = head->next;
+    struct list_head *fast = head->next;
+
+    while (fast != head && fast->next != head) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    element_t *middle_element = list_entry(slow, element_t, list);
+
+    list_del(slow);
+
+    free(middle_element->value);
+    free(middle_element);
+
     return true;
 }
 
